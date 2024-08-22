16dbebeecf0b3f8eb3da121bf4306d4c6e4dc1db
YewonKimMe <kimyewon030428@gmial.com>
2024-08-22 23:43:26 +0900

    traineddata-kor

--- src/main/java/ai/agreement/AiProject/controller/AiController.java
+++ src/main/java/ai/agreement/AiProject/controller/AiController.java
@@ -41,25 +41,25 @@ public class AiController {
 
     private final OcrService ocrService;
 
-    final String requirement = "다음 계약서에 대한 평가를 수행해 주세요.\n" +
-            "기준:\n" +
-            "1. 기본 정보의 정확성 (20점 만점)\n" +
-            "2. 법적 효력 (20점 만점)\n" +
-            "3. 특약 사항의 완성도 (20점 만점)\n" +
-            "4. 양측 권리와 의무의 명확성 (20점 만점)\n" +
-            "5. 기타 중요한 조항 (20점 만점)\n" +
-            "출력 방법 설명:"+
-            "계약서 제목을 간단하게 언급하고, 임대인은(회사)는 누구인지 언급해주세요. 각 항목의 점수를 구체적으로 설명 및 제공해 주세요. 평가내용은 높임말을 사용합니다." +
-            "응답 양식은 다음과 같습니다. 응답 양식을 철저하게 지켜주세요. AI가 생성한 응답은 {} 내에 위치하고, 중괄호'{}'는 출력하지 않습니다. 계약서 OCR 내용은 출력하지 않습니다." +
-            "{계약서 제목}\n" +
-            "{임대인 정보}\n" +
-            "1. 기본 정보의 정확성 (20점 만점)\n{1항 평가내용}" +
-            "2. 법적 효력 (20점 만점)\n{2항 평가내용}" +
-            "3. 특약 사항의 완성도 (20점 만점)\n{3항 평가내용}" +
-            "4. 양측 권리와 의무의 명확성 (20점 만점)\n{4항 평가내용}" +
-            "5. 기타 중요한 조항 (20점 만점)\n{5항 평가내용}" +
-            "총평: {총평 내용}\n" +
-            "@{1항 평가 점수;2항 평가 점수;3항 평가 점수;4항 평가 점수;5항 평가 점수}";
+//    final String requirement = "다음 계약서에 대한 평가를 수행해 주세요.\n" +
+//            "기준:\n" +
+//            "1. 기본 정보의 정확성 (20점 만점)\n" +
+//            "2. 법적 효력 (20점 만점)\n" +
+//            "3. 특약 사항의 완성도 (20점 만점)\n" +
+//            "4. 양측 권리와 의무의 명확성 (20점 만점)\n" +
+//            "5. 기타 중요한 조항 (20점 만점)\n" +
+//            "출력 방법 설명:"+
+//            "계약서 제목을 간단하게 언급하고, 임대인은(회사)는 누구인지 언급해주세요. 각 항목의 점수를 구체적으로 설명 및 제공해 주세요. 평가내용은 높임말을 사용합니다." +
+//            "응답 양식은 다음과 같습니다. 응답 양식을 철저하게 지켜주세요. AI가 생성한 응답은 {} 내에 위치하고, 중괄호'{}'는 출력하지 않습니다. 계약서 OCR 내용은 출력하지 않습니다." +
+//            "{계약서 제목}\n" +
+//            "{임대인 정보}\n" +
+//            "1. 기본 정보의 정확성 (20점 만점)\n{1항 평가내용}" +
+//            "2. 법적 효력 (20점 만점)\n{2항 평가내용}" +
+//            "3. 특약 사항의 완성도 (20점 만점)\n{3항 평가내용}" +
+//            "4. 양측 권리와 의무의 명확성 (20점 만점)\n{4항 평가내용}" +
+//            "5. 기타 중요한 조항 (20점 만점)\n{5항 평가내용}" +
+//            "총평: {총평 내용}\n" +
+//            "@{1항 평가 점수;2항 평가 점수;3항 평가 점수;4항 평가 점수;5항 평가 점수}";
 
     @GetMapping
     public ResponseEntity<ResultAndData> getDefaultChat(@RequestBody DefaultChatMessage defaultChatMessage) {
@@ -74,7 +74,7 @@ public class AiController {
                         .build())
                 .chatMemory(chatMemory)
                 .build();
-        String generatedMessage = assistant.chat(requirement +" 계약서 내용: "+defaultChatMessage.getMessage());
+        String generatedMessage = assistant.chat("계약서 내용: "+defaultChatMessage.getMessage());
         log.debug("gen message={}", generatedMessage);
         return ResponseEntity
                 .ok()
@@ -88,7 +88,7 @@ public class AiController {
         Assistant assistant = this.setAssistant("gpt-4o-mini");
 
         String convertedString = ocrService.doImageOcr(file);
-        String generatedMessage = assistant.chat(requirement +" 계약서 OCR 내용: " + convertedString);
+        String generatedMessage = assistant.chat("계약서 OCR 내용: " + convertedString);
         return ResponseEntity.
                 ok()
                 .body(generatedMessage);
