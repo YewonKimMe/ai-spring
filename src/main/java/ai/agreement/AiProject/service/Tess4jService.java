07cf0baaf049d9c66f05781a6cb0f5b691a2c3f3
YewonKimMe <kimyewon030428@gmial.com>
2024-08-22 23:44:35 +0900

    Tess4jService

---
+++ src/main/java/ai/agreement/AiProject/service/Tess4jService.java
@@ -0,0 +1,54 @@
+package ai.agreement.AiProject.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class Tess4jService implements OcrService {
+
+    private final Tesseract tesseract = setTesseract();
+
+//    @Value("${ocr.tessdata}")
+//    private String tessdataPrefix;
+
+    public String doImageOcr(MultipartFile file) {
+        // MultipartFile을 BufferedImage로 변환
+        BufferedImage bufferedImage = convertMultipartFileToBufferedImage(file);
+        try {
+            return tesseract.doOCR(bufferedImage);
+        } catch (TesseractException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Tesseract setTesseract() {
+        Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath("src/main/resources/tessdata"); //** 학습된데이터가 있는 폴더를 지정해준다.
+        tesseract.setLanguage("kor"); // 언어설정
+        tesseract.setPageSegMode(1); // 페이지 모드 설정
+        tesseract.setOcrEngineMode(1);
+
+        return tesseract;
+    }
+
+    private BufferedImage convertMultipartFileToBufferedImage(MultipartFile file) {
+        try {
+            return ImageIO.read(file.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
