6c0913bc7b0b3e19a721ca9f5a14dc26e219d309
YewonKimMe <kimyewon030428@gmial.com>
2024-08-23 09:07:44 +0900

    doImagesOcr

--- src/main/java/ai/agreement/AiProject/service/OcrService.java
+++ src/main/java/ai/agreement/AiProject/service/OcrService.java
@@ -2,8 +2,12 @@ package ai.agreement.AiProject.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface OcrService {
 
     String doImageOcr(MultipartFile file);
+
+    String doImagesOcr(List<MultipartFile> files);
 
 }
