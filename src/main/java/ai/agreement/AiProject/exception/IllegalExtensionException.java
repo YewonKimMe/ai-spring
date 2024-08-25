318987cfd170f117d18dcff980562ba77a3d5716
YewonKimMe <kimyewon030428@gmial.com>
2024-08-26 05:48:32 +0900

    IllegalExtensionException

---
+++ src/main/java/ai/agreement/AiProject/exception/IllegalExtensionException.java
@@ -0,0 +1,7 @@
+package ai.agreement.AiProject.exception;
+
+public class IllegalExtensionException extends RuntimeException {
+    public IllegalExtensionException(String message) {
+        super(message);
+    }
+}
