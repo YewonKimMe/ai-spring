547ac9835f746f1c80a767b29556f475869c5941
YewonKimMe <kimyewon030428@gmial.com>
2024-08-26 05:56:17 +0900

    edit description

--- src/main/java/ai/agreement/AiProject/config/SwaggerConfig.java
+++ src/main/java/ai/agreement/AiProject/config/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig {
     private Info apiInformation() {
         return new Info()
                 .title("OCR 기반 계약서 평가 AI")
-                .description("prompt 기반의 계약서 이미지 ocr 추출 후 평가<br><br>API 문서 테스트-Authorize 에 API KEY 입력<br><br>프론트 앱-Http 요청 시 Authorization 헤더에 API KEY 포함")
+                .description("prompt 기반의 계약서 이미지 ocr 추출 후 평가<br><br>API 문서: Authorize 에 API KEY 입력<br><br>프론트 앱: Http 요청 시 Authorization 헤더에 API KEY 포함<br><br>OCR Version: google-cloud-vision:3.46.0")
                 .version("1.0.0");
     }
 }
