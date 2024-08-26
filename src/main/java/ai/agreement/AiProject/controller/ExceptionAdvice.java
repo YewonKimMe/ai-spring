e4ba74d7c4c21001d3da415ee26328dc136fcc6d
YewonKimMe <kimyewon030428@gmial.com>
2024-08-27 01:20:29 +0900

    IllegalContractTypeException

--- src/main/java/ai/agreement/AiProject/controller/ExceptionAdvice.java
+++ src/main/java/ai/agreement/AiProject/controller/ExceptionAdvice.java
@@ -2,14 +2,18 @@ package ai.agreement.AiProject.controller;
 
 import ai.agreement.AiProject.dto.response.FailAndData;
 import ai.agreement.AiProject.dto.response.ResultAndData;
+import ai.agreement.AiProject.exception.IllegalContractTypeException;
 import ai.agreement.AiProject.exception.IllegalExtensionException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,6 +27,17 @@ public class ExceptionAdvice {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .contentType(MediaType.APPLICATION_JSON)
+                .body(errorResultAndMessage);
+    }
+
+    @ExceptionHandler(IllegalContractTypeException.class)
+    public ResponseEntity<ResultAndData> illegalContractTypeException(IllegalContractTypeException e) {
+        log.debug("IllegalExtensionException={}", e.getMessage());
+        ResultAndData errorResultAndMessage = new FailAndData(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .cacheControl(CacheControl.maxAge(3, TimeUnit.SECONDS))
                 .body(errorResultAndMessage);
     }
 }
