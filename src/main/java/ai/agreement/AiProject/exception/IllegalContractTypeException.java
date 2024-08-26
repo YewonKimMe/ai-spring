304b1d1b8dc9fe171c1405bea3ec54a4b9a9e383
YewonKimMe <kimyewon030428@gmial.com>
2024-08-27 01:20:52 +0900

    IllegalContractTypeException

---
+++ src/main/java/ai/agreement/AiProject/exception/IllegalContractTypeException.java
@@ -0,0 +1,7 @@
+package ai.agreement.AiProject.exception;
+
+public class IllegalContractTypeException extends RuntimeException {
+    public IllegalContractTypeException(String message) {
+        super(message);
+    }
+}
