0100628c832b5e7b2a6dfa583dd9c1ea27c514f0
YewonKimMe <kimyewon030428@gmial.com>
2024-08-28 00:30:53 +0900

    exception check

--- src/main/java/ai/agreement/AiProject/service/GoogleVisionOcrService.java
+++ src/main/java/ai/agreement/AiProject/service/GoogleVisionOcrService.java
@@ -1,6 +1,9 @@
 package ai.agreement.AiProject.service;
 
+import ai.agreement.AiProject.enums.ErrorMessage;
 import ai.agreement.AiProject.exception.IllegalExtensionException;
+import ai.agreement.AiProject.exception.ImageOcrException;
+import ai.agreement.AiProject.exception.TextNotFoundException;
 import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
 import lombok.extern.slf4j.Slf4j;
@@ -70,7 +73,7 @@ public class GoogleVisionOcrService implements OcrService {
             log.debug("extension={}", extension);
 
             if (!List.of(allowedExtensions).contains(extension)) {
-                throw new IllegalExtensionException("올바르지 않은 파일 확장자 입니다.\n전송된 확장자: " + extension + ", 파일명: " + fileName);
+                throw new IllegalExtensionException(ErrorMessage.ILLEGAL_EXTENSION.getErrorMessage() + "\n전송된 확장자: " + extension + ", 파일명: " + fileName);
             }
         }
 
@@ -79,7 +82,7 @@ public class GoogleVisionOcrService implements OcrService {
         try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
 
             for (MultipartFile file : files) {
-                byte[] data = file.getBytes();
+                byte[] data = file.getBytes(); // Mulitipartfile 을 byte array 로 변환
 
                 ByteString imgBytes = ByteString.copyFrom(data);
 
@@ -89,26 +92,30 @@ public class GoogleVisionOcrService implements OcrService {
 
                 AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
 
-                requests.add(request);
+                requests.add(request); // 요청 리스트에 추가
             }
 
-            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests); // 이미지 추출 실행
             List<AnnotateImageResponse> responses = response.getResponsesList();
 
             StringBuilder sb = new StringBuilder();
 
             for (AnnotateImageResponse res : responses) {
-                if (res.hasError()) {
+                if (res.hasError()) { // 이미지 응답에 오류가 있을 경우
                     log.error("Error: {}", res.getError().getMessage());
-                    return null;
+                    throw new ImageOcrException(ErrorMessage.IMAGE_OCR.getErrorMessage());
+                }
+                if (res.getTextAnnotationsList().isEmpty()) { // 식별된 텍스트가 존재하지 않을 경우
+                    throw new TextNotFoundException(ErrorMessage.TEXT_NOT_FOUND.getErrorMessage());
                 }
                 for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-                    sb.append(annotation.getDescription());
+                    sb.append(annotation.getDescription()); // 추출된 텍스트를 StringBuilder 에 추가
                 }
             }
-
             return sb.toString();
 
+        } catch (TextNotFoundException | ImageOcrException e) {
+            throw e;
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
