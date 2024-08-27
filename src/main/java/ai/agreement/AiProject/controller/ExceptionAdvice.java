faf44ca41768542e489d6ed8e40df11ce2140540
YewonKimMe <kimyewon030428@gmial.com>
2024-08-28 00:30:13 +0900

    TextNotFoundException

--- src/main/java/ai/agreement/AiProject/controller/ExceptionAdvice.java
+++ src/main/java/ai/agreement/AiProject/controller/ExceptionAdvice.java
@@ -4,6 +4,7 @@ import ai.agreement.AiProject.dto.response.FailAndData;
 import ai.agreement.AiProject.dto.response.ResultAndData;
 import ai.agreement.AiProject.exception.IllegalContractTypeException;
 import ai.agreement.AiProject.exception.IllegalExtensionException;
+import ai.agreement.AiProject.exception.TextNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.CacheControl;
@@ -32,6 +33,17 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(IllegalContractTypeException.class)
     public ResponseEntity<ResultAndData> illegalContractTypeException(IllegalContractTypeException e) {
+        log.debug("IllegalExtensionException={}", e.getMessage());
+        ResultAndData errorResultAndMessage = new FailAndData(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .cacheControl(CacheControl.maxAge(3, TimeUnit.SECONDS))
+                .body(errorResultAndMessage);
+    }
+
+    @ExceptionHandler(TextNotFoundException.class)
+    public ResponseEntity<ResultAndData> textNotFoundException(TextNotFoundException e) {
         log.debug("IllegalExtensionException={}", e.getMessage());
         ResultAndData errorResultAndMessage = new FailAndData(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
         return ResponseEntity
