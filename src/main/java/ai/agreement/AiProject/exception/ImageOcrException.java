3d64f5c8b01069a115f47ae19464b9e2d81a2adf
YewonKimMe <kimyewon030428@gmial.com>
2024-08-28 00:30:59 +0900

    ImageOcrException

---
+++ src/main/java/ai/agreement/AiProject/exception/ImageOcrException.java
@@ -0,0 +1,7 @@
+package ai.agreement.AiProject.exception;
+
+public class ImageOcrException extends RuntimeException {
+    public ImageOcrException(String message) {
+        super(message);
+    }
+}
