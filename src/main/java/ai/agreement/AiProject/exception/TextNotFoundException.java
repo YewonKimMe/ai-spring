d0072cce3edf54660fa990ab4381ba44ffb7a100
YewonKimMe <kimyewon030428@gmial.com>
2024-08-28 00:31:15 +0900

    TextNotFoundException

---
+++ src/main/java/ai/agreement/AiProject/exception/TextNotFoundException.java
@@ -0,0 +1,8 @@
+package ai.agreement.AiProject.exception;
+
+public class TextNotFoundException extends RuntimeException {
+
+    public TextNotFoundException(String message) {
+        super(message);
+    }
+}
