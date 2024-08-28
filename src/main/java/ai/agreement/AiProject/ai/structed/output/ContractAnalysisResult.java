ac0fbfc8e67ccfb1e95ee80671634a2f0c83de25
YewonKimMe <kimyewon030428@gmial.com>
2024-08-28 20:56:24 +0900

    overallReview

--- src/main/java/ai/agreement/AiProject/ai/structed/output/ContractAnalysisResult.java
+++ src/main/java/ai/agreement/AiProject/ai/structed/output/ContractAnalysisResult.java
@@ -1,7 +1,14 @@
 package ai.agreement.AiProject.ai.structed.output;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class ContractAnalysisResult {
 
     private String contractName;
@@ -11,6 +18,8 @@ public class ContractAnalysisResult {
     private List<String> evaluationContentsByStandard; // 각 항목 기준별 평가 코멘트
 
     private List<Integer> evaluationScoresByStandard; //  각 항목 기준별 평가 점수
+
+    private String overallReview; // 총평
 
     private boolean isContract; // 계약서 유무 확인
 
