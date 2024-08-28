5a251e680a87c2af9c73e986e2c02bf4bc990090
YewonKimMe <kimyewon030428@gmial.com>
2024-08-28 20:56:35 +0900

    createAnalysisResult

--- src/main/java/ai/agreement/AiProject/service/GenAIService.java
+++ src/main/java/ai/agreement/AiProject/service/GenAIService.java
@@ -1,6 +1,11 @@
 package ai.agreement.AiProject.service;
 
+import ai.agreement.AiProject.ai.StructuredContractAnalysisGenerator;
+import ai.agreement.AiProject.ai.structed.output.ContractAnalysisResult;
+
 public interface GenAIService {
 
     String createChatRequest(String message);
+
+    ContractAnalysisResult createAnalysisResult(String message);
 }
