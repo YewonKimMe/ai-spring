361d7308ce80d440844606ccb93d8a531eddc53c
YewonKimMe <kimyewon030428@gmial.com>
2024-08-28 13:49:49 +0900

    isContract

--- src/main/java/ai/agreement/AiProject/ai/structed/output/ContractAnalysisResult.java
+++ src/main/java/ai/agreement/AiProject/ai/structed/output/ContractAnalysisResult.java
@@ -6,9 +6,12 @@ public class ContractAnalysisResult {
 
     private String contractName;
 
-    private String landlord;
+    private String landlord; // 임대인 (회사 또는 이름)
 
     private List<String> evaluationContentsByStandard; // 각 항목 기준별 평가 코멘트
+
     private List<Integer> evaluationScoresByStandard; //  각 항목 기준별 평가 점수
+
+    private boolean isContract; // 계약서 유무 확인
 
 }
