65871a19e58cafafb09885a4c81e291921f4c6c6
YewonKimMe <kimyewon030428@gmial.com>
2024-08-30 00:48:36 +0900

    StringResult

---
+++ src/main/java/ai/agreement/AiProject/ai/result/StringResult.java
@@ -0,0 +1,11 @@
+package ai.agreement.AiProject.ai.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StringResult implements Result {
+
+    private String content;
+}
